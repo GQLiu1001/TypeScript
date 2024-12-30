1d475bbda5a0fbbf66e019a8ed6126e3c1e2360a
GQLiu1001 <moondust1196551872@gmail.com>
2024-12-30 15:32:02 +0800

    20241230

--- src/index.ts
+++ src/index.ts
@@ -180,3 +180,93 @@ console.log(num1%2? "1" : '0')
 console.log(num2%2? "1":"0")
 
 
+
+//装饰器 是一个方法（函数） 用在类的注解上一个@ 只有一个参数target
+function Demo(target:Function){
+    console.log(target)
+}
+@Demo
+class Person1 {
+    constructor(public name : string,public age : number) {}
+}
+//new 表示可以用new操作 ...args构造器可以接受任意数量的参数
+//any[]构造器可以接受任意类型参数 {}返回类型是对象 (非NULL 非UNDEFINED)
+type Constructor = new (...args:any[]) => {};
+//传入的T必须满足Constructor
+function LogTime<T extends Constructor>(target:T){
+    //返回的新类具有原本所有的属性
+    return class extends target {
+        createdTime:Date
+        constructor(...args:any[]) {
+            super(...args);
+            this.createdTime = new Date();
+        }
+        getTime(){
+            return `创建时间是${this.createdTime}`;
+        }
+    }
+}
+
+@LogTime
+class Person2 {
+    constructor(public name : string,public age : number) {}
+}
+interface Person2 {
+    getTime():void
+}
+let pp2 =new Person2('小明',14)
+console.log(pp2)
+console.log(pp2.getTime())
+
+//装饰器工厂 return返回一个装饰器
+function LogInfo(n:number){
+    return function (target:Function){
+        target.prototype.intro = function (){
+            for (let number = 0; number < n; number++) {
+                console.log(`我是${this.name} ${this.age} `);
+            }
+        }
+    }
+}
+@LogInfo(3)
+class Person3 {
+    constructor(public name : string,public age : number) {}
+    introduce():void{}
+}
+interface Person3 {
+    intro():void
+}
+let  pp3 = new Person3('小李',20)
+pp3.intro()
+//类上可以组合工厂装饰器 工厂从上到下 装饰器从下到上
+
+//类装饰器   ↑ 拓展功能方法 拓展属性 target.prototype
+//属性装饰器  ↓
+function Demo1(target:object,propertyKey:string){
+    //target原型对象或类本身 对于静态属性是类，对于实例属性是类的原型对象
+    // propertyKey 属性名
+
+}
+//age 一改 就要知道变化 用实例对象解决闭包问题
+function State(target:object,propertyKey:string){
+    let key = `__${propertyKey}`;
+    Object.defineProperty(target,propertyKey,{
+        get(){
+            return this[key];
+        },
+        set(val:any){
+            console.log(`age的最新值为${val}`)
+            this[key] = val
+        }
+    })
+}
+class Person4 {
+    @Demo1 name : string
+    @State age : number
+    @Demo1 static school:string
+    constructor( name : string, age : number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const pp = new Person4('晓燕',88)
